4e0c7d059ac07fbdadfd4f7667b0109d3d87d63b
Rupam852 <rupambairagya08@gmail.com>
2026-06-23 03:02:01 +0530

    Fix Google OAuth scopes config

--- web/src/contexts/AuthContext.jsx
+++ web/src/contexts/AuthContext.jsx
@@ -68,7 +68,7 @@ export function AuthProvider({ children }) {
       provider: 'google',
       options: {
         redirectTo: `${window.location.origin}/auth/callback`,
-        scopes: 'userinfo.profile userinfo.email drive.file drive.readonly',
+        scopes: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/drive.file',
       }
     })
     if (error) throw error
