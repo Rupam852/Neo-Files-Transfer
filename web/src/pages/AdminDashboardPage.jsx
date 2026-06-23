bf51d7d7edefbc24628a99d994a4558afecca982
Rupam852 <rupambairagya08@gmail.com>
2026-06-24 00:44:41 +0530

    style: add extra bottom padding on admin dashboard

--- web/src/pages/AdminDashboardPage.jsx
+++ web/src/pages/AdminDashboardPage.jsx
@@ -479,7 +479,7 @@ export default function AdminDashboardPage() {
         </div>
       </header>
 
-      <div className="max-w-7xl mx-auto p-4 lg:p-6">
+      <div className="max-w-7xl mx-auto p-4 lg:p-6 pb-24">
         {/* Stats */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-6">
           <StatCard icon={Users} label="Total Registrations" value={pendingCount + approvedCount} color="blue" />
